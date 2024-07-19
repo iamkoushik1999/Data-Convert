e1c2aee76e18a59e8c51a64a6d3622f2853f620d
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-07-19 22:11:44 +0530

    Add: Error Middleware

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ import cors from 'cors';
 // Database
 import connectDB from './connection/database.js';
 connectDB();
+// Error Middleware
+import { errorHandler } from './middlewares/errorMiddleware.js';
 
 const app = express();
 
@@ -14,5 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors('*'));
+
+app.use(errorHandler);
 
 export default app;

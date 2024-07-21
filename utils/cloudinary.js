84d4914ace2fa7bbb644a02fa5138f2c8ee97104
iamkoushik1999 <iamkoushik1999@gmail.com>
2024-07-21 22:20:40 +0530

    Add: Cloudinary

---
+++ utils/cloudinary.js
@@ -0,0 +1,25 @@
+// Packages
+import multer from 'multer';
+import cloudinary from 'cloudinary';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+// ENV
+const { CLOUDINARY_CLOUDNAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET } =
+  process.env;
+// ------------------------------------------------
+
+// Cloudinary Config
+cloudinary.config({
+  cloud_name: CLOUDINARY_CLOUDNAME,
+  api_key: CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_API_SECRET,
+});
+
+// Multer storage configuration
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'File Convert',
+  },
+});
+
+exports.upload = multer({ storage: storage });
